02b36deb28cd8b894b24b721711ada275ee7957d
Runweztt <amarikwat@gmail.com>
2026-02-21 13:25:58 +0200

    fix: update CORS for production

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from routes.expiry import bp as expiry_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:5173"])
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     app.register_blueprint(profile_bp)
     app.register_blueprint(members_bp)
